ce5b74bfc87471d1b67d3037b7c6b71302a68209
msousa <msousa.dev@outlook.com>
2022-08-23 21:03:33 -0300

    conect db

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const { response } = require('express');
 const express = require('express');
 const app = express();
+const db = require('./db/connection');
+
 const PORT = 3000;
 
 app.listen(PORT, function() {
     console.log(`O servidor está ativo na porta ${PORT}`);
 });
 
+//conexao com banco
+db.authenticate()
+    .then(() => {
+        console.log('conectado');
+    })
+    .catch(err => {
+        console.log('error: ', err);
+    })
+
+
+//rotas
 app.get('/' , (require, response) => {
     response.send("HelloJob");
 });
