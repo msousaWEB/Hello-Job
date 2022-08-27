2bd041578edd48fcd5f5dc5ddcea45c5eb24b260
msousa <msousa.dev@outlook.com>
2022-08-27 10:37:53 -0300

    job view vacancies

--- routes/jobRouter.js
+++ routes/jobRouter.js
@@ -1,10 +1,21 @@
+const { response } = require('express');
 const express = require('express');
 const router = express.Router();
 const Job = require('../models/Job');
-
+//form add
 router.get('/add', (require, response) => {
     response.render('add');
 })
+
+//detalhe da vaga
+router.get('/view/:id', (require, response) => Job.findOne({
+    where: {id: require.params.id}
+}).then(job => {
+    response.render('view', {
+        job
+    });
+}).catch(err => console.log(err))
+);
 
 router.post('/add', (require, response) => {
     let {title, salary, company, description, email, new_job} = require.body;
